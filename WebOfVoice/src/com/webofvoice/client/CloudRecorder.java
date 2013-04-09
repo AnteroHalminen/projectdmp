c3df5aaa44dc9e368a99718644e9029fac1db55c
Antero Halminen <antero.halminen@aalto.fi>
2013-04-09 13:24:40 +0300

    Add push-to-talk button + basic recorder class (TODO: the cloud part...)

---
+++ WebOfVoice/src/com/webofvoice/client/CloudRecorder.java
@@ -0,0 +1,74 @@
+package com.webofvoice.client;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.content.Context;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+public class CloudRecorder {
+	private static final String TAG = "CloudRecorder";
+	private MediaRecorder mediaRecorder = null;
+	private File outputFile = null;
+	
+	public CloudRecorder() {
+	}
+	
+	public void startRecording(Context currentContext) {
+		
+		if (null != outputFile) {
+			Log.e(TAG, "spurious call to startRecording before stopRecording");
+			return;
+		}
+		File outputDir = currentContext.getCacheDir(); // context being the Activity pointer
+		outputFile = null; 
+		try {
+			outputFile = File.createTempFile("wovRec", ".3gp", outputDir);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Log.e(TAG, "failed to create temporary file for sample (in " + outputDir.getPath() + ")");
+			return;
+		}
+		String outputFileName = outputFile.getPath();
+		Log.v(TAG, "recording to " + outputFileName);
+
+		mediaRecorder = new MediaRecorder();
+		mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFile(outputFileName);
+
+		try {
+			mediaRecorder.prepare();
+			mediaRecorder.start();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+            outputFile.delete();
+        }
+	}
+
+	public void stopRecording() {
+		Log.v(TAG, "recording complete");
+
+		if (null != mediaRecorder) {
+			try {
+				mediaRecorder.stop();
+			} catch(RuntimeException e) {
+				Log.e(TAG, "MediaRecorder.stop() failed!");
+			} finally {
+				mediaRecorder.reset();
+				mediaRecorder.release();
+				mediaRecorder = null;
+			}
+		}
+
+		if (null != outputFile) {
+
+			// TODO: Send the file @ outputFileName to cloud or wherever
+			
+			outputFile.delete();
+	        outputFile = null;
+		}
+	}
+}
--- WebOfVoice/src/com/webofvoice/client/MainActivity.java
+++ WebOfVoice/src/com/webofvoice/client/MainActivity.java
@@ -4,16 +4,23 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.gcm.GCMRegistrar;
 
 public class MainActivity extends Activity {
 	public static String SENDER_ID = "5613935475";
 	public static String TAG = "MainActivity";
+	private CloudRecorder cloudRecorder = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
+		
 		GCMRegistrar.checkDevice(this);
 		GCMRegistrar.checkManifest(this);
 		final String regId = GCMRegistrar.getRegistrationId(this);
@@ -22,7 +29,27 @@ public class MainActivity extends Activity {
 		} else {
 		    Log.v(TAG, "Already registered");
 		}
-	}
+		
+		cloudRecorder = new CloudRecorder();
+
+		final Button tangent = (Button) findViewById(R.id.tangentButton);
+        tangent.setOnTouchListener(new View.OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					cloudRecorder.startRecording(v.getContext());
+					return true;
+				case MotionEvent.ACTION_UP:
+					cloudRecorder.stopRecording();
+					return true;
+				default:
+					return false;
+				}
+			}
+		});
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
